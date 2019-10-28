fe9ae5b9f9ea75bd8e0c82b753ab77b45f59403a
Anthony Garitachea <lgaritachea45204@gmail.com>
2019-10-28 15:12:53 -0700

    sets up server and added 3 successfull get requests

--- server/app.js
+++ server/app.js
@@ -1,8 +1,43 @@
 const express = require('express');
 const morgan = require('morgan');
+var bodyParser = require('body-parser');
 
 const app = express();
 
-// add your code here
+app.use(morgan('dev'));
+
+let mock = 
+[
+    {
+      todoItemId: 0,
+      name: 'an item',
+      priority: 3,
+      completed: false
+    },
+    {
+      todoItemId: 1,
+      name: 'another item',
+      priority: 2,
+      completed: false
+    },
+    {
+      todoItemId: 2,
+      name: 'a done item',
+      priority: 1,
+      completed: true
+    }
+];
+
+app.get('/', (req, res) => {
+    res.send({'status': 'ok'})
+});
+
+app.get('/api/TodoItems', (req, res) => {
+  res.send(mock)
+});
+
+app.get('/api/TodoItems/:id', (req, res) => {
+  res.send(mock[req.params.id])
+});
 
 module.exports = app;
